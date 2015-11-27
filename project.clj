42ae29acb906ff8c651450f30ec1367a126acb4c
Tyler van Hensbergen <tvanhens@gmail.com>
2015-11-26 20:57:06 -0500

    0.1.0 Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nw/lein-template "0.1.0-SNAPSHOT"
+(defproject nw/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
